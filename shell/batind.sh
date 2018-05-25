1b747bfdc9bc34237c2043a25782ead92c0b4e35
prinzpiuz <prinzpiuz@disroot.org>
2018-05-25 17:18:51 +0000

    display export error

--- shell/batind.sh
+++ shell/batind.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+export DISPLAY=:0
 batperct=$(upower -i $(upower -e | grep '/battery') | grep  -E "percentage:")
 bat=${batperct:24:26}
 intbat=$(echo $bat | cut -d'%' -f1)
 if [ "$intbat" -lt "15" ]; then
-    notify-send 'battery low!!!!!!!!!!!!!! please plugin charger charge is only' "$intbat"    
+    notify-send 'battery low!!!!!!!!!!!!!! please plugin charger..... charge is only' "$intbat"    
 fi   
-
 
 
