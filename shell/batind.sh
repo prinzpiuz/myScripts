0400771918d3fcca9b31805e0d6fc2fecbcddea9
prinzpiuz <prinzpiuz@disroot.org>
2018-08-01 16:42:55 +0000

    added functionality for checking charging state

--- shell/batind.sh
+++ shell/batind.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 export DISPLAY=:0
+state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state" )
+stat=${state:25:32}
 batperct=$(upower -i $(upower -e | grep '/battery') | grep  -E "percentage:")
 bat=${batperct:24:26}
 intbat=$(echo $bat | cut -d'%' -f1)
-if [ "$intbat" -lt "15" ]; then
-    notify-send 'battery low!!!!!!!!!!!!!! please plugin charger..... charge is only' "$intbat"    
+if [ "$intbat" -lt "15" -a "$stat" == "discharging" ]; then
+    notify-send 'battery low..! please plugin charger..... charge is only' "$intbat"    
 fi   
 
 
